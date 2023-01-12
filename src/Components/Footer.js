2c4122d6a8f71bdbb9d05db18875422085f371b8
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-12 13:14:37 +0300

    editing footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,32 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import EmailIcon from "@material-ui/icons/Email";
 import TelegramIcon from "@material-ui/icons/Telegram";
 import "./Footer.css";
+import PersonIcon from "@material-ui/icons/Person";
+import PhoneIcon from "@material-ui/icons/Phone";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import logo from "../Assets/logo.png";
 
 function Footer() {
   return (
     <div className="footer">
+      <div className="footer_logo">
+        <img src={logo} alt="logo" />
+        <h2>BIRUK BROKER WORKS</h2>
+      </div>
+      <div>
+        <div className="info">
+          <PersonIcon />
+          <p>Biruk Sisay</p>
+        </div>
+        <div className="info">
+          <PhoneIcon />
+          <p>+251963137715</p>
+        </div>
+        <div className="info">
+          <LocationOnIcon />
+          <p>Addis Ababa, Yeka, Kotebe</p>
+        </div>
+      </div>
       <div>
         <a href="https://www.facebook.com/">
           <FacebookIcon className="icon" />
