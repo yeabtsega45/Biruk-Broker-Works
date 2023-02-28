a5379442bec02526e5f8ded2658601c94c66cb37
yeabtsega45 <yabtsega2022@gmail.com>
2023-02-28 23:43:57 +0300

    deleting some containers and hooks

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import { useRef } from "react";
 import { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo3.png";
-// import SearchIcon from "@material-ui/icons/Search";
 import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from "@material-ui/icons/Close";
 
 function Navbar() {
   const [show, setShow] = useState(true);
 
-  // const navRef = useRef();
-
-  // const showNavbar = () => {
-  //   navRef.current.classList.toggle("resposive_nav");
-  // };
-
   return (
     <div className="navbar">
       <div className="logo">
         <img src={logo} alt="logo" />
-        {/* <h2>BIRUK BROKER WORKS</h2> */}
       </div>
-      {/* <div className="navandsearch"> */}
       <nav
         className={show ? "nav-links" : "nav-links-mobile"}
         onClick={() => setShow(true)}
@@ -36,11 +26,6 @@ function Navbar() {
       <button className="mobile-menu-icon" onClick={() => setShow(!show)}>
         {show ? <MenuIcon /> : <CloseIcon />}
       </button>
-      {/* <div className="search">
-          <input placeholder="search" type="text" />
-          <SearchIcon className="searchicon" />
-        </div> */}
-      {/* </div> */}
     </div>
   );
 }
