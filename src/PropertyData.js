30a76807941d35c1bf81ee413d48c62ad8d86f92
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-24 00:27:32 +0300

    creating a data file

---
+++ src/PropertyData.js
@@ -0,0 +1,23 @@
+import img1 from "../Assets/frontpage.jpg";
+import img11 from "../Assets/frontpage2.webp";
+import img2 from "../Assets/house1.1.jpg";
+import img3 from "../Assets/house1.2.jpg";
+import img4 from "../Assets/house1.3.jpg";
+// import img5 from "../Assets/house2.jpg";
+import img6 from "../Assets/house3.jpg";
+import img7 from "../Assets/g+1.jpg";
+import img8 from "../Assets/g+2.jpg";
+import img9 from "../Assets/g+3.jpg";
+
+const PropertyData = [
+  {
+    image: img2,
+    image2: img3,
+    image3: img4,
+    type: "town house",
+    location: "Bole Wolosefer Addis Ababa",
+    area: "150 m2",
+    rooms: "beds:3, bath:1",
+    price: "2,500,000 br",
+  },
+];
