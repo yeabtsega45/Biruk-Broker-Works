79de8944421f24ef76ea510c19284413c23dae12
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-18 18:08:39 +0300

    connecting with database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const connectDb = require("./config/connectDb");
 
 const app = express();
+connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use("/images", express.static("public/images"));
 
 app.listen(process.env.PORT, () =>
   console.log("server has been started successfully!")
