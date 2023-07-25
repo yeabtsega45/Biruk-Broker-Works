e566d372b3d739a14dfc0937284ee10dce8842f0
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-25 14:09:42 +0300

    creating api to update a property

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -29,3 +29,25 @@ propertyController.post("/", verifyToken, async (req, res) => {
     return res.status(500).json(error);
   }
 });
+
+// update property
+propertyController.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const property = await Property.findById(req.params.id);
+    if (property.currentOwner.toString() !== req.user.id) {
+      throw new Error(
+        "You are not allowed to update other people's properties"
+      );
+    }
+
+    const updatedProperty = await Property.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedProperty);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
