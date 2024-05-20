d40a0d0b6251809392bbb8db646a313eb90c2ba4
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-20 11:58:16 +0300

    update footer

--- frontend/src/Components/BottomBar.js
+++ frontend/src/Components/BottomBar.js
@@ -3,6 +3,7 @@ import "./BottomBar.css";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import EmailIcon from "@material-ui/icons/Email";
 import TelegramIcon from "@material-ui/icons/Telegram";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function BottomBar() {
   return (
@@ -14,8 +15,14 @@ function BottomBar() {
         </div>
         <div className="footer-section">
           <h3>Contact Us</h3>
-          <p>Email: BirukSisay@gmail.com</p>
-          <p>Phone: +251963137715</p>
+          <div>
+            <PhoneIcon />
+            <a href="tel:+251963137715">+251963137715</a>
+          </div>
+          <p>
+            Email:{""}
+            <a href="mailto:BirukSisay@gmail.com">BirukSisay@gmail.com</a>
+          </p>
         </div>
         <div className="footer-section">
           <h3>Follow Us</h3>
