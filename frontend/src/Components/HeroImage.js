1a4304ce172f71bbdf17f0e7473480841aa3c938
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-30 22:44:36 +0300

    add styling

--- frontend/src/Components/HeroImage.js
+++ frontend/src/Components/HeroImage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import img11 from "../Assets/frontpage2.webp";
 import "./HeroImage.css";
+import heroImage from "../Assets/HeroImage.jpg";
 
 function HeroImage(props) {
   return (
     <div className="hero">
-      <img src={img11} alt="frontpage" />
+      <img src={heroImage} alt="frontpage" />
       <h1>{props.title}</h1>
     </div>
   );
