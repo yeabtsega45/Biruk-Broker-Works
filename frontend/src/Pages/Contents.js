4e611159af3c0d64787d894dffbc1d2e1e081e15
yeabtsega45 <yabtsega2022@gmail.com>
2024-06-04 21:43:26 +0300

    add contents page

---
+++ frontend/src/Pages/Contents.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import contentData from "./contentData";
+import ContentCard from "./ContentCard";
+
+function Contents() {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path);
+  };
+  return (
+    <div className="contents-container">
+      <div className="contents-section">
+        <h1 className="contents-title">Our Contents</h1>
+        <hr className="contents-divider" />
+
+        <div className="contents-grid">
+          {contentData.map((content, index) => (
+            <ContentCard
+              key={index}
+              image={content.image}
+              title={content.title}
+              description={content.description}
+              onClick={() => handleClick(content.navigateTo)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contents;
