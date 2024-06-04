5bd89693b660dbf86e9e1d80e3abd9875aaf7515
yeabtsega45 <yabtsega2022@gmail.com>
2024-06-04 21:50:15 +0300

    add contents page

---
+++ frontend/src/Utils/contentData.js
@@ -0,0 +1,26 @@
+import content1 from "../../assets/content-1.svg";
+import content2 from "../../assets/content-2.svg";
+import content3 from "../../assets/content-3.svg";
+
+const contentData = [
+  {
+    image: content1,
+    title: "Course Study",
+    description: "Study on specific bible topics.",
+    navigateTo: "/courses",
+  },
+  {
+    image: content2,
+    title: "Sabbath School Study",
+    description: "Quarterly bible study on a topic.",
+    navigateTo: "/sabbathSchool",
+  },
+  {
+    image: content3,
+    title: "Devotional Study",
+    description: "Daily devotional text study.",
+    navigateTo: "/devotion",
+  },
+];
+
+export default contentData;
