deafa354876467b78da059ea20a7f362fb3f4a9c
yeabtsega45 <yabtsega2022@gmail.com>
2024-06-07 17:29:03 +0300

    add new component

---
+++ frontend/src/Components/TeamMemberCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TeamMemberCard({ photo, name, role }) {
+  return (
+    <div className="team-member">
+      <img className="member-photo" src={photo} alt={name} />
+      <div className="member-name">{name}</div>
+      <div className="member-role">{role}</div>
+    </div>
+  );
+}
+
+export default TeamMemberCard;
